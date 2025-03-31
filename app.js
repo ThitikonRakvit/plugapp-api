18eb57a9c43b60d2d72ea4b7a2d5f65d332c3e4c
Thitikon Rakvit <thitikon.rakvit@gmail.com>
2025-03-31 21:14:47 +0700

    add: getCar getStation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
+const geolib = require("geolib");
 
 const corsOptions = {
   origin: "*",
@@ -132,6 +133,67 @@ app.get("/charging-stations", async (req, res) => {
     console.error(error);
     res.status(500).json({
       message: "Error fetching charging stations",
+      error: error.message,
+    });
+  }
+});
+
+const userLocation = {
+  latitude: 13.73037296485693,
+  longitude: 100.7800772848435,
+};
+
+app.get("/charging-stations/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const chargingStation = await prisma.chargingStation.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!chargingStation) {
+      return res.status(404).json({ message: "Charging station not found" });
+    }
+
+    const stationLocation = {
+      latitude: chargingStation.latitude,
+      longitude: chargingStation.longitude,
+    };
+
+    const distance = geolib.getDistance(userLocation, stationLocation);
+
+    res.status(200).json({
+      message: "Charging station found",
+      data: chargingStation,
+      distance: `${(distance / 1000).toFixed(2)} km.`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error fetching charging station",
+      error: error.message,
+    });
+  }
+});
+
+app.get("/car/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const car = await prisma.car.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+
+    res.status(200).json({
+      message: "Car details fetched successfully",
+      data: car,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error fetching car details",
       error: error.message,
     });
   }
